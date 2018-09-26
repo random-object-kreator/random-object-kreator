c36453c2120283aaacce8dab7f736ba5bf47785c
memoizr <memoizrlabs@gmail.com>
2018-09-26 16:01:23 +0100

    Add more customization parameters

--- src/main/kotlin/ro/kreator/PropertyBased.kt
+++ src/main/kotlin/ro/kreator/PropertyBased.kt
@@ -3,6 +3,7 @@ package ro.kreator
 import ro.kreator.CreationLogic.hash
 import ro.kreator.CreationLogic.instantiateRandomClass
 import ro.kreator.CreationLogic.with
+import javax.swing.text.html.HTML.Tag.I
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KType
@@ -21,11 +22,13 @@ fun forAll(numberOfRuns: Int = 100, block: PropertyBased.() -> Unit) {
     numberOfRuns.times { Property(currentMethod.methodName.hash with it.hash, map).block() }
 }
 
-internal class Property(token: Token, countOfInvocations: MutableMap<KType, Int>) : PropertyBased(token,countOfInvocations)
+internal class Property(token: Token, countOfInvocations: MutableMap<KType, Int>) : PropertyBased(token, countOfInvocations)
 
-sealed class PropertyBased(val token: Token, val countOfInvocations: MutableMap<KType, Int>): Reify() {
+sealed class PropertyBased(val token: Token, val countOfInvocations: MutableMap<KType, Int>) : Reify() {
 
-    fun KType.store() = this.apply { countOfInvocations[this]?.let { countOfInvocations[this] = it.inc() } ?: { countOfInvocations[this] = 0 }() }
+    fun KType.store() = this.apply {
+        countOfInvocations[this]?.let { countOfInvocations[this] = it.inc() } ?: { countOfInvocations[this] = 0 }()
+    }
 
     fun KType.tokenize(token: Token) = token with (countOfInvocations[this]?.hash ?: 0)
 
@@ -74,9 +77,10 @@ abstract class Reify {
             ))
 }
 
-class Creator(val type: KType, token: Token): PropertyBased(token, mutableMapOf()) {
+class Creator(val type: KType, token: Token) : PropertyBased(token, mutableMapOf()) {
 
     object any
+
     fun <T> any(): T = any as T
 
     operator inline fun <reified A, reified R : Any> ((A) -> R).get(a: A = any()): R {
@@ -136,11 +140,263 @@ class Creator(val type: KType, token: Token): PropertyBased(token, mutableMapOf(
         )
     }
 
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified RR : Any>
+            ((A, B, C, D, E, F, G) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token)
+        )
+    }
+
+
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified RR : Any>
+            ((A, B, C, D, E, F, G, H) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token)
+        )
+    }
+
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token)
+        )
+    }
+
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token)
+        )
+    }
+
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token)
+        )
+    }
+
+    NOPQRS
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token)
+        )
+    }
+
+    OPQRS
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified N, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M, N) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any(), n: N = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class, N::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token),
+                n or new<N>(params[13], token)
+        )
+    }
+
+    PQRS
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified N, reified O, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any(), n: N = any(), o: O = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class, N::class, O::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token),
+                n or new<N>(params[13], token),
+                o or new<O>(params[14], token)
+        )
+    }
+
+    QRS
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified N, reified O, reified P, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any(), n: N = any(), o: O = any(), p: P = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class, N::class, O::class, P::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token),
+                n or new<N>(params[13], token),
+                o or new<O>(params[14], token),
+                p or new<P>(params[15], token)
+        )
+    }
+
+    RS
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified N, reified O, reified P, reified Q, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any(), n: N = any(), o: O = any(), p: P = any(), q: Q = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class, N::class, O::class, P::class, Q::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token),
+                n or new<N>(params[13], token),
+                o or new<O>(params[14], token),
+                p or new<P>(params[15], token),
+                q or new<Q>(params[16], token)
+        )
+    }
+
+    S
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified N, reified O, reified P, reified Q, reified R, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any(), n: N = any(), o: O = any(), p: P = any(), q: Q = any(), r: R = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class, N::class, O::class, P::class, Q::class, R::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token),
+                n or new<N>(params[13], token),
+                o or new<O>(params[14], token),
+                p or new<P>(params[15], token),
+                q or new<Q>(params[16], token),
+                r or new<R>(params[17], token)
+        )
+    }
+
+
+    operator inline fun <reified A, reified B, reified C, reified D, reified E, reified F, reified G, reified H, reified I, reified J, reified K, reified L, reified M, reified N, reified O, reified P, reified Q, reified R, reified S, reified RR : Any>
+            ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S) -> RR).get(a: A = any(), b: B = any(), c: C = any(), d: D = any(), e: E = any(), f: F = any(), g: G = any(), h: H = any(), i: I = any(), j: J = any(), k: K = any(), l: L = any(), m: M = any(), n: N = any(), o: O = any(), p: P = any(), q: Q = any(), r: R = any(), s: S = any()): RR {
+        val params = getParameters(type, A::class, B::class, C::class, D::class, E::class, F::class, G::class, H::class, I::class, J::class, K::class, L::class, M::class, N::class, O::class, P::class, Q::class, R::class, S::class)
+        return invoke(
+                a or new<A>(params[0], token),
+                b or new<B>(params[1], token),
+                c or new<C>(params[2], token),
+                d or new<D>(params[3], token),
+                e or new<E>(params[4], token),
+                f or new<F>(params[5], token),
+                g or new<G>(params[6], token),
+                h or new<H>(params[7], token),
+                i or new<I>(params[8], token),
+                j or new<J>(params[9], token),
+                k or new<K>(params[10], token),
+                l or new<L>(params[11], token),
+                m or new<M>(params[12], token),
+                n or new<N>(params[13], token),
+                o or new<O>(params[14], token),
+                p or new<P>(params[15], token),
+                q or new<Q>(params[16], token),
+                r or new<R>(params[17], token),
+                s or new<S>(params[18], token)
+        )
+    }
+
     fun <R : Any> getConstructor(klass: KClass<R>) = klass.constructors.filter { !it.parameters.any { (it.type.jvmErasure == klass) } }.toList()
 
     infix fun <T> T.or(other: T) = if (this == any) other else this
 
-fun getParameters(type: KType, vararg classes: KClass<*>): List<KType> {
+    fun getParameters(type: KType, vararg classes: KClass<*>): List<KType> {
         val klass = type.jvmErasure
         val generics = type.arguments.map { it.type }
         val constructors = getConstructor(klass)
