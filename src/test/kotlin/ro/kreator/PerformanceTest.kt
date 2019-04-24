9e6a9dfeb29d0aab025483f9bd22e94757946c6c
memoizr <memoizrlabs@gmail.com>
2019-04-24 18:30:33 +0100

    Improve performance

--- src/test/kotlin/ro/kreator/PerformanceTest.kt
+++ src/test/kotlin/ro/kreator/PerformanceTest.kt
@@ -1,18 +1,16 @@
 package ro.kreator
 
-import org.apache.commons.lang3.RandomStringUtils
 import org.junit.Test
-import java.util.*
 import kotlin.system.measureTimeMillis
 
 class PerformanceTest {
-    val a0 by aRandomListOf<ManyParams>(10000)
+    val a0 by aRandomListOf<ManyParams>(10)
 
 
     @Test
     fun isFast(){
         measureTimeMillis {
-            a0.count()
+            println(a0)
         }.print()
     }
 
@@ -24,9 +22,9 @@ class PerformanceTest {
     }
 
 
-    private fun pseudoRandom(token: Long): Random = Random(Seed.seed with token)
-    internal infix fun Long.with(other: Long): Long = this * 31 + other
-    private fun Random.int(bound: Int) = if (bound == 0) 0 else nextInt(bound)
+//    private fun pseudoRandom(token: Long): Random = Random(Seed.seed with token)
+//    internal infix fun Long.with(other: Long): Long = this * 31 + other
+//    private fun Random.int(bound: Int) = if (bound == 0) 0 else nextInt(bound)
 }
 
 
@@ -34,8 +32,8 @@ data class Yo(val a: String, val b: Int, val c: Int)
 data class I(val x: Yo, val b: Yo, val d: Yo)
 data class ManyParams(val a: I, val b: I, val c: I, val d: I, val e: I, val f: I, val g: I, val h: I)
 
-fun getString() = Random(0).let { RandomStringUtils.random(Math.max(1, it.nextInt(9)), 0, 6000, true, true, null, it) }
-fun getInt() = Random(0).nextInt()
-fun getYo()= Yo::class.constructors.first().call(getString(), getInt(), getInt())
-fun getI()= I::class.constructors.first().call(getYo(), getYo(), getYo())
-fun getMP()= ManyParams::class.constructors.first().call(getI(), getI(), getI(), getI(), getI(), getI(), getI(), getI())
+//fun getString() = Random(0).let { RandomStringUtils.random(Math.max(1, it.nextInt(9)), 0, 6000, true, true, null, it) }
+//fun getInt() = Random(0).nextInt()
+//fun getYo()= Yo::class.constructors.first().call(getString(), getInt(), getInt())
+//fun getI()= I::class.constructors.first().call(getYo(), getYo(), getYo())
+//fun getMP()= ManyParams::class.constructors.first().call(getI(), getI(), getI(), getI(), getI(), getI(), getI(), getI())
